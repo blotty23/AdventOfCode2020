be32b1aefde7ebd65c3424d59cb22cb29af860f7
Emma Blott <blott23@gmail.com>
2020-12-10 17:12:37 +0000

    create day 3 files

---
+++ spec/AdventofCode3_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../AdventofCode3'
+
+context Expenses do
+  describe "" do
+    it "" do
+      # Arrange
+      
+      # Act
+      result = 
+      # Assert
+      expect(result).to eq()
+    end
+  end 
